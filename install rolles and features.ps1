bcb798ba2dca5556cb4e70df5f9dec349d8d3005
debeerst louis <louis.debeerst@student.howest.be>
2022-09-20 17:57:56 +0200

    Create install rolles and features.ps1

---
+++ install rolles and features.ps1
@@ -0,0 +1,3 @@
+﻿Install-WindowsFeature -Name AD-Domain-Services -ComputerName DC1 -Restart
+Install-WindowsFeature -Name dns -ComputerName DC1 -Restart
+Install-WindowsFeature -Name dhcp server -ComputerName DC1 -Restart
