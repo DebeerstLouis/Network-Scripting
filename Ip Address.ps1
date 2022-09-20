94bfe81a33fdfead6a7858d4eca383886f9641bc
debeerst louis <louis.debeerst@student.howest.be>
2022-09-20 11:29:20 +0200

    Update Ip Address.ps1

--- Ip Address.ps1
+++ Ip Address.ps1
@@ -1,2 +1,4 @@
-New-NetIPAddress –IPAddress 192.168.1.2 -DefaultGateway 192.168.1.1 -PrefixLength 24 -InterfaceIndex (Get-NetAdapter).InterfaceIndex
-Set-DNSClientServerAdress –InterfaceIndex (Get-NetAdapter).InterfaceIndex –ServerAddresses 172.20.4.140,172.20.4.141
+Set-NetIPInterface -Dhcp Disabled -InterfaceIndex (Get-NetAdapter).InterfaceIndex
+New-NetIPAddress -IPAddress 192.168.1.2 -DefaultGateway 192.168.1.1 -PrefixLength 24 -InterfaceIndex (Get-NetAdapter).InterfaceIndex
+
+Set-DnsClientServerAddress -InterfaceAlias Ethernet0 -ServerAddresses ("172.20.4.140","172.20.4.141")
