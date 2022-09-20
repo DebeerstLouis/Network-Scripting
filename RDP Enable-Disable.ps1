88e67e5415ab80e6a4e33a070b6be9b2c1533d99
debeerst louis <louis.debeerst@student.howest.be>
2022-09-20 10:55:41 +0200

    Create RDP

---
+++ RDP Enable-Disable.ps1
@@ -0,0 +1,13 @@
+﻿$item=get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fDenyTsConnections" 
+if ($item.fDenyTsconnections)
+{
+    write-host "Enable RDP "
+    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name  "fDenyTsConnections" -Value 0
+    Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+}
+else
+{
+    write-host "Enable RDP "
+    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name  "fDenyTsConnections" -Value 1
+    Disable-NetFirewallRule -DisplayGroup "Remote Desktop"
+}
