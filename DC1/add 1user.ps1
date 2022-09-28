865e4087800494e6d3d301e015123303c0c43d16
debeerst louis <louis.debeerst@student.howest.be>
2022-09-28 10:06:13 +0200

    Update add 1user.ps1

--- DC1/add 1user.ps1
+++ DC1/add 1user.ps1
@@ -2,3 +2,6 @@
 # ADD 1 user 
 #
 New-ADUser -Name Rozemieke -AccountPassword (ConvertTo-SecureString "P@ssword" -AsplainText -Force) -DisplayName Rozemieke -GivenName Rozemieke -HomeDirectory \\ms\homedirs\%username% -HomeDrive H: -PasswordNeverExpires $true -Path "OU=Secretariaat,OU=Mijn school,DC=intranet,DC=mijnschool,DC=be" -SamAccountName Rozemieke -ScriptPath login.bat -Enabled $true
+#
+# parameters veranderen!
+#
