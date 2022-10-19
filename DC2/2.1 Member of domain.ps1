9eb312e17dceb9f40606a560b507f18ab6927e2a
debeerst louis <louis.debeerst@student.howest.be>
2022-10-19 08:53:34 +0200

    Create 2.1 Member of domain.ps1

---
+++ DC2/2.1 Member of domain.ps1
@@ -0,0 +1,11 @@
+﻿$PC_Name = "DC2"
+
+#Connecting to Remote PS on DC2
+$s = New-PSSession -ComputerName $PC_Name -Credential "INTRANET\Administrator" 
+
+Invoke-Command -Session $s -ScriptBlock  {
+    $Domain = "intranet.mijnschool.be"
+
+    Write-Host "Adding machine to domain $Domain"
+    Add-Computer -DomainName $Domain -Restart
+}
