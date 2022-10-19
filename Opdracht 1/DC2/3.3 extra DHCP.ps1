a0f2cb542aa1f825f8730231e5b4a281bd75408e
debeerst louis <louis.debeerst@student.howest.be>
2022-10-19 09:17:23 +0200

    Create 3.3 extra DHCP.ps1

---
+++ Opdracht 1/DC2/3.3 extra DHCP.ps1
@@ -0,0 +1,23 @@
+﻿$PC_Name = "DC2"
+
+#remote connection to DC2
+$s = New-PSSession -ComputerName $PC_Name -Credential "INTRANET\Administrator" 
+
+Invoke-Command -Session $s -ScriptBlock {
+    # Variables
+    $PrimaryDHCPServer = "DC1.intranet.mijnschool.be"
+    $DNS_Name2 = "DC2.intranet.mijnschool.be"
+    $Scope = "192.168.1.0"
+    $Password = "P@ssw0rd"
+
+    $IP_Type = "IPV4"
+    $IP_Int_Alias = "Ethernet0"
+
+    $IP_Address = (Get-NetIPAddress -AddressFamily $IP_Type -InterfaceAlias $IP_Int_Alias).IPAddress
+
+    # Configuring the DHCP Server on Dc2
+    Add-DhcpServerInDC -IPAddress $IP_Address -DnsName $DNS_Name2 
+
+    # Creating a Failover to the DHCP Server on DC1
+    Add-DhcpServerv4Failover -ComputerName $PrimaryDHCPServer -Name "SFO-SIN-Failover" -PartnerServer $DNS_Name2 -ScopeId $Scope -SharedSecret $Password
+}
